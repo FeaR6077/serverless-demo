f93db519c491e3346bac90af36d37952c5c14b3e
FeaR6077 <julian@aflorithmic.ai>
2020-10-15 15:21:14 +0200

    delete user

--- user.py
+++ user.py
@@ -1,10 +1,9 @@
 import boto3
 
+
 def get_user(user_id):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('julian-test')
     response = table.get_item(Key={'userId': user_id})
     item = response['Item']
     return item
-    
-    
