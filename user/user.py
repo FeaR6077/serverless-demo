893d583131d43c0d842f77809e77238ea7a83b34
FeaR6077 <julian@aflorithmic.ai>
2020-10-16 17:46:55 +0200

    correction of delete function

--- user/user.py
+++ user/user.py
@@ -11,14 +11,9 @@ class User:
         except:
             return False
             
-    def get_user(user_id):
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('julian-test')
-        response = table.get_item(Key={'userId': user_id})
-        try:
-            return response["Item"]
-        except:
-            return False
+    def delete_user(user_id):
+        dynamodb = boto3.client('dynamodb')
+        dynamodb.delete_item(TableName='julian-test',Key={'userId':{"S": user_id}})
 
     def add_user(user_id, user_name):
         dynamodb = boto3.client('dynamodb')
